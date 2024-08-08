1dd5d43d5c0e295a20432a7c716c03c738a0c6af
InduBhusanNath <ibnhost@gmail.com>
2024-08-08 15:43:34 +0530

    Eight Push

--- frontend/src/Pages/header.jsx
+++ frontend/src/Pages/header.jsx
@@ -1,8 +1,9 @@
 export default function Header(){
      return(
          <>
-           <h1>Header</h1>
-           <a href="/aboutus">Lift</a>
+          <h1>Header</h1>
          </>
      );
+
 }
+         
